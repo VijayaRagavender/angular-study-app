f684f3a9237d56ffe8d8a35b20db542dc8730430
Vijaya Ragavender G S <vijaya.ragavender@nielsen.com>
2019-05-22 22:55:20 +0530

    My Study App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { FavouriteChangedInterface } from './favorite/favorite.component';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,65 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  post = {
+    isFavorite : true,
+    isReact : true
+  }
+   tweet = {
+    isLiked : false,
+    likesCount : 0
+  }
+
+  course = [1,2];
+  semester = [1,2];
+  viewMode : string ;
+  resources;
+  canSave = false;
+  samText : string;
+  samOpText : string;
+  employees = [
+    {code : 'Ops01' , dept : 'Operation'},
+    {id : 'Dev01', dept : 'Dev' },
+    {id : 'Tes01', dept : 'Test'} 
+  ];
+  
+  onAdd(){
+    this.employees.push({code : 'SUP' , dept:'SUPPORT'});
+  }
+
+  loadResources(){
+    this.resources = [
+      {name:'RES1'},
+      {name:'RES2'},
+      {name:'RES3'}
+    ];
+  }
+
+  task = {
+    title : 'tweet',
+    assignee : null
+  }
+
+  onKeyPress(){
+    this.samOpText = this.samText.toUpperCase();
+    return this.samOpText;
+  }
+  onClickSaveBtn(){
+    this.canSave = !this.canSave;
+  }
+  trackResource(index , resources){
+    return resources ? resources.name : undefined; 
+  }
+  onRemove(emp){
+    let index = this.employees.indexOf(emp);
+    console.log(index);
+    this.employees.splice(index , 1);
+  }
+  onFavoriteChanged(eventArgs : FavouriteChangedInterface) {
+    console.log('favorite changed ', eventArgs);
+  }
+
+  Reacted(eventArgs : FavouriteChangedInterface) {
+    console.log("Reacted for the post" , eventArgs)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,113 @@
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GitHubServices } from './services/gitHub.services';
+import { PostService } from './services/post.service';
+import { TitleCasingPipe } from './titlecasing/titlecasing.pipe';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { UppPipe } from './upp.pipe';
+import { SummaryPipe } from './summary.pipe';
+import { FitnessComponent } from './fitness.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors.service';
+import { AuthorService } from './author.service';
+import { AuthorComponent } from './author.component';
+import { EmailService } from './email.service';
+import { CoursesService } from './courses.service';
+import { CoursesComponent } from './courses.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { ErrorHandler } from '@angular/core';
+import { AppErrorHandler } from '@angular/common/app-error-handler';
+
 
 import { AppComponent } from './app.component';
+import { NavigationbarComponent } from './navigationbar/navigationbar.component';
+import { TitlecasingComponent } from './titlecasing/titlecasing.component';
+import { PanelComponent } from './panel/panel.component';
+import { LikesComponent } from './likes/likes.component';
+import { InputFormatDirective } from './input-format.directive';
+import { ZippyComponent } from './zippy/zippy.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { CourseFormComponent } from './course-form/course-form.component';
+import { SignupFormComponent } from "./signup-form/signup-form.component";
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+import { ChangePasswordFormComponent } from './change-password-form/change-password-form.component';
+import { HttpPostsComponent } from './http-posts/http-posts.component';
+import { GithubFollowersComponent } from './github-followers/github-followers.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent, 
+    NavigationbarComponent,
+    AuthorComponent,
+    AuthorsComponent,
+    FitnessComponent,
+    SummaryPipe,
+    UppPipe,
+    FavoriteComponent,
+    TitlecasingComponent,
+    TitleCasingPipe,
+    PanelComponent,
+    LikesComponent,
+    InputFormatDirective,
+    ZippyComponent,
+    ContactFormComponent,
+    CourseFormComponent,
+    SignupFormComponent,
+    NewCourseFormComponent,
+    ChangePasswordFormComponent,
+    HttpPostsComponent,
+    GithubFollowersComponent,
+    NavbarComponent,
+    HomeComponent,
+    NotFoundComponent,
+    GithubProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path : '', 
+        component : HomeComponent 
+      },
+      {
+        path :'followers/:id/:username', /* /:username denotes query parameter which will be generated dynamically in run time*/
+        component : GithubProfileComponent 
+      },
+      {
+        path :'followers', 
+        component : GithubFollowersComponent
+      },
+      {
+        path :'posts', 
+        component : HttpPostsComponent
+      },
+      {
+        path :'**', 
+        component : NotFoundComponent 
+      },
+
+    ])
   ],
-  providers: [],
+  providers: [
+    PostService,
+    CoursesService,
+    EmailService,
+    AuthorService,
+    AuthorsService,
+    GitHubServices,
+    {provide : ErrorHandler , useClass : AppErrorHandler}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
